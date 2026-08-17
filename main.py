c7a011bab10723f146d8648a8f04f4c83c00d1d1
ron45700 <ron.mintz21@gmail.com>
2026-08-17 18:56:41 +0300

    Initialcommit: project structure, venv setup, greeter module

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.greeter import greet, print_greet
+
+
+if __name__ == "__main__":
+    print_greet("ron")
+    print(greet("ron.m"))
---
+++ src/greeter.py
@@ -0,0 +1,11 @@
+
+def print_greet(name):
+    print(f"greeting {name}!")
+
+def greet(name):
+    return f"Lets greeting {name}"
+
+if __name__ == "__main__":
+    print(greet("ron.m"))
+
+    print_greet("ron")
